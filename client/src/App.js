f7dfb25d1af97f606d8b64417aaf0e18e84a8359
unknown <audcortes@hotmail.com>
2023-02-24 12:19:18 -0500

    fornt inicial

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import { Link } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+    <div>
       <header>
-       <a href="/">Amazone</a>
+       <Link to="/">Amazone</Link>
       </header>
       <main>
-        list products
+      <Routes>
+        <Route path="/product/:slug" element={<ProductScreen />}/>
+        <Route path="/" element={<HomeScreen/>}/>
+      </Routes>
       </main>
     </div>
+    </BrowserRouter>
   );
 }
 
